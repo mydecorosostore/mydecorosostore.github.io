39cbc004481e293865ad3812d7defd7a68b91587
unknown <adenshepiano@gmail.com>
2023-03-22 05:57:00 +0300

    third commit

--- index.js
+++ index.js
@@ -6,19 +6,22 @@ window.onload = () => {
   let currentIndex = 1
 
   async function FadeSection1Image () {
+    currentIndex++
     const img = document.getElementById('section-1-img')
     const p = document.getElementById('section-1-p')
+    const h1 = document.getElementById('section-1-h1')
     img.classList.add('fade')
     p.classList.add('fade')
+    h1.classList.add('fade')
     await sleep(2500)
     img.src = `images/image${currentIndex}.png`
-    currentIndex++
     if (currentIndex >= 9) {
       currentIndex = 1
     }
     await sleep(2500)
     img.classList.remove('fade')
     p.classList.remove('fade')
+    h1.classList.remove('fade')
   }
 
   FadeSection1Image()
